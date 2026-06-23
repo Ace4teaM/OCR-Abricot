871ce756f730f101eff6df54ec2eb7bebde19b22
Thomas Auguey <thomas.auguey@hotmail.com>
2026-06-23 16:56:39 +0200

    Affiche les données de la page Projects

--- src/app/(site)/projects/page.js
+++ src/app/(site)/projects/page.js
@@ -1,9 +1,65 @@
+"use client";
+
 import styles from "./page.module.css";
 import {Button} from "@/components/Buttons";
 import {ProjectCard} from "@/components/Cards";
+import { useEffect, useState } from "react";
+import {useFetch} from "@/hooks/Http";
 
 export default function Projects() {
-  
+  /*
+  {
+  "success": true,
+  "message": "Projets récupérés avec succès",
+  "data": {
+    "projects": [
+      {
+        "id": "cmqf60f5j000tijekkatb1j8u",
+        "name": "Plateforme de Formation",
+        "description": "Système de gestion de cours en ligne avec vidéos, quiz et suivi des progrès.",
+        "createdAt": "2026-06-15T12:04:00.824Z",
+        "updatedAt": "2026-06-15T12:04:00.824Z",
+        "ownerId": "cmqf60f4d0009ijek4s1ecg6r",
+        "owner": {
+          "id": "cmqf60f4d0009ijek4s1ecg6r",
+          "email": "jacques@example.com",
+          "name": "Jacques Durand"
+        },
+        "members": [
+          {
+            "id": "cmqf60f5q000vijek76sq3r0j",
+            "role": "CONTRIBUTOR",
+            "joinedAt": "2026-06-15T12:04:00.831Z",
+            "userId": "cmqf60enh0000ijekbaoghiy4",
+            "projectId": "cmqf60f5j000tijekkatb1j8u",
+            "user": {
+              "id": "cmqf60enh0000ijekbaoghiy4",
+              "email": "alice@example.com",
+              "name": "Alice Martin"
+            }
+          }
+        ],
+        "_count": {
+          "tasks": 3
+        },
+        "userRole": "CONTRIBUTOR"
+      },
+      ...
+    ]
+  }
+}
+  */
+  const projects = useFetch("projects", process.env.NEXT_PUBLIC_USER_API_URL)
+
+  useEffect(()=>{
+    if(projects.hasData == false || projects.data.length == 0)
+      return
+
+    const data = projects;
+
+    console.log("data", data)
+  }, [projects.hasData])
+
   return (
     <div className={styles.page}>
       <header className={styles.header}>
@@ -19,15 +75,9 @@ export default function Projects() {
       </header>
       <section>
         <div className={styles.grid}>
-          <ProjectCard></ProjectCard>
-          <ProjectCard></ProjectCard>
-          <ProjectCard></ProjectCard>
-          <ProjectCard></ProjectCard>
-          <ProjectCard></ProjectCard>
-          <ProjectCard></ProjectCard>
-          <ProjectCard></ProjectCard>
-          <ProjectCard></ProjectCard>
-          <ProjectCard></ProjectCard>
+           {projects.hasData === true && projects.data.data.projects.map((project)=>
+              <ProjectCard {...project}></ProjectCard>
+           )}
         </div>
       </section>
     </div>
--- src/components/Cards/ProjectCard/ProjectCard.jsx
+++ src/components/Cards/ProjectCard/ProjectCard.jsx
@@ -5,14 +5,39 @@ import {TagLabel, UserLabel} from '@/components/Labels'
 import {Users} from 'lucide-react';
 
 const ProjectCard = ({
+  id= "cmqf60f5j000tijekkatb1j8u",
+  name= "Plateforme de Formation",
+  description= "Système de gestion de cours en ligne avec vidéos, quiz et suivi des progrès.",
+  createdAt= "2026-06-15T12:04:00.824Z",
+  updatedAt= "2026-06-15T12:04:00.824Z",
+  ownerId= "cmqf60f4d0009ijek4s1ecg6r",
+  owner= {
+    id: "cmqf60f4d0009ijek4s1ecg6r",
+    email: "jacques@example.com",
+    name: "Jacques Durand"
+  },
+  Members= [
+    {
+      id: "cmqf60f5q000vijek76sq3r0j",
+      role: "CONTRIBUTOR",
+      joinedAt: "2026-06-15T12:04:00.831Z",
+      userId: "cmqf60enh0000ijekbaoghiy4",
+      projectId: "cmqf60f5j000tijekkatb1j8u",
+      user: {
+        id: "cmqf60enh0000ijekbaoghiy4",
+        email: "alice@example.com",
+        name: "Alice Martin"
+      }
+    }
+  ],
   ...props
 }) => {
 
   return (
     <BaseCard {...props}>
       <header>
-        <h2>Nom du projet</h2>
-        <p>Développement de la nouvelle version de l'API REST avec authentification JWT</p>
+        <h2>{name}</h2>
+        <p>{description}</p>
       </header>
       <section>
         <div className={styles.progressInfos}>
@@ -27,11 +52,12 @@ const ProjectCard = ({
           <Users height={16} fill="gray"></Users> Equipe (3)
         </div>
         <div className={styles.users}>
-          <UserLabel color="orange"></UserLabel>
+          <UserLabel color="orange" {...owner}></UserLabel>
           <TagLabel color="orange">Propriétaire</TagLabel>
           <div className={styles.usersGroup}>
-            <UserLabel withBorder={true} color="gray" firstname="Billy" lastname="Dupont"></UserLabel>
-            <UserLabel withBorder={true} color="gray" firstname="Caroline" lastname="Vincent"></UserLabel>
+            {Members.map((member)=>{
+              <UserLabel withBorder={true} color="gray" {...member.user}></UserLabel>
+            })}
           </div>
         </div>
       </footer>
