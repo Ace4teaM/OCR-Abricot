d52ca99e08dca765923273effda96f76ca2381aa
Thomas Auguey <thomas.auguey@hotmail.com>
2026-06-23 16:01:12 +0200

    ErrorMessage : cast to string

--- src/components/Parts/ErrorMessage/ErrorMessage.jsx
+++ src/components/Parts/ErrorMessage/ErrorMessage.jsx
@@ -9,8 +9,7 @@ const ErrorMessage = ({
 
   const message = typeof(data) === "string" ? data
                     : data.message ? data.message
-                    : data instanceof Response && data.status != 200 ? `Un problème est survenu avec le serveur. ${data.message}`
-                   : "";
+                   : `${data}`;
 
   console.log("message", data)
 
